f4726d1231fa471f0ba79eabbd0094245875cb44
GreenColoredLife <greencoloredlife@gmail.com>
2019-12-29 22:26:12 +0700

    Added login and registration

--- express_backend/server.js
+++ express_backend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 // Used for setting environmental variables
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -14,19 +18,27 @@ app.use(
 );
 
 // Database config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Make a connection to MongoDB server
 mongoose
-    .connect(
-        db,
-        { useNewUrlParser: true }
-    )
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Successfully connected to MongoDB"))
     .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes using /api/users set to direct to users file
+app.use("/api/users", users);
+
 // Set port as environmental variable or fallback as port 5000
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Set app to run on port
-app.listen(port, () => console.log(`Server started and running on port ${port}`));
+app.listen(port, () =>
+    console.log(`Server started and running on port ${port}`)
+);
---
+++ express_backend/validation/register.js
@@ -0,0 +1,39 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+    // Set errors to empty object
+    let errors = {};
+
+    // Convert empty fields to an empty string to use validator functions
+    data.email = !isEmpty ? data.email : "";
+    data.password = !isEmpty ? data.password : "";
+    data.password2 = !isEmpty ? data.password2 : "";
+
+    // Email checks
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+
+    // Password checks
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = "Confirm password field is required";
+    }
+    if (!Validator.isLength(data.password, { min: 6, max: 12 })) {
+        errors.password = "Password must be between 6 to 12 characters";
+    }
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match";
+    }
+
+    // Return object of errors and validity-check boolean
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
