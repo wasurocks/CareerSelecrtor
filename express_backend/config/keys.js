7436cda2596f4506051f8fe5691361c6194c05bd
GreenColoredLife <greencoloredlife@gmail.com>
2020-01-06 23:01:21 +0700

    Completed backend + set env variables

--- express_backend/config/keys.js
+++ express_backend/config/keys.js
@@ -1,6 +1,6 @@
 // Export URI for connection to database
 module.exports = {
-    mongoURI: `mongodb+srv://wrocket:fIEdC85JidFjFiTF@cluster0-vjl21.mongodb.net/FoodSelectorDB?retryWrites=true&w=majority`,
+    mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-vjl21.mongodb.net/FoodSelectorDB?retryWrites=true&w=majority`,
     secretOrKey: "secret"
 };
 
