fd7e3de54ec512a6ca8def5f13bdde8d87835eb9
GreenColoredLife <greencoloredlife@gmail.com>
2020-01-06 23:11:10 +0700

    Minor fix

--- express_backend/config/keys.js
+++ express_backend/config/keys.js
@@ -1,6 +1,6 @@
 // Export URI for connection to database
 module.exports = {
     mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-vjl21.mongodb.net/FoodSelectorDB?retryWrites=true&w=majority`,
-    secretOrKey: "secret"
+    secretOrKey: process.env.SECRETORKEY
 };
 
