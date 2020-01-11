a436f934160dd77546dd242ffcdfa1252282a344
GreenColoredLife <greencoloredlife@gmail.com>
2020-01-12 00:56:43 +0700

    Refactored registration form + provided route

--- react_frontend/src/components/RegisterForm.js
+++ react_frontend/src/components/RegisterForm.js
@@ -25,124 +25,137 @@ const theme = createMuiTheme({
     }
 });
 
-const RegisterForm = () => (
-    <Formik
-        initialValues={{ email: "", password: "", password2: "" }}
-        onSubmit={
-            // Define a function for what happens on submit
-            (values, { setSubmitting }) => {
-                setTimeout(() => {
-                    console.log("Registering ", values);
-                    const config = {
-                        headers: {
-                            "Content-Type": "application/x-www-form-urlencoded"
-                        }
-                    };
-                    axios
-                        .post(
-                            `http://localhost:8000/api/users/register`,
-                            qs.stringify(values),
-                            config
+export default class RegisterForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isRegistered: false
+        };
+    }
+
+    handleSubmit = async (values, { setSubmitting }) => {
+        setTimeout(() => {
+            console.log("Registering ", values);
+            const config = {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            };
+            axios
+                .post(
+                    `http://localhost:8000/api/users/register`,
+                    qs.stringify(values),
+                    config
+                )
+                .then(res => {
+                    if (res.status == 201) {
+                        console.log("Success");
+                        this.setState({isRegistered:true});
+                    }
+                })
+                .catch(err => {
+                    if (err.response.data.email == "User already exists") {
+                        console.log("User already exists");
+                    } else {
+                        // Should never happen but just in case
+                        console.log("Invalid information");
+                    }
+                });
+            setSubmitting(false);
+        }, 500);
+    };
+
+    render() {
+        if(this.state.isRegistered) return <Redirect to="/success"/>
+        return (
+            <Formik
+                initialValues={{ email: "", password: "", password2: "" }}
+                onSubmit={this.handleSubmit}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string()
+                        .email("Invalid email")
+                        .required("Email is required")
+                        .matches(
+                            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                            "Invalid email"
+                        ),
+                    password: Yup.string()
+                        .required("Password is required")
+                        .min(6, "Password must be between 6-12 characters")
+                        .max(12, "Password must be between 6-12 characters"),
+                    password2: Yup.string()
+                        .required("Confirm password is required")
+                        .oneOf(
+                            [Yup.ref("password"), null],
+                            "Passwords must match"
                         )
-                        .then(res => {
-                            if(res.status == 201){
-                                console.log("Success");
-                            } else {
-                                console.log(res.data);
-                            }
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                    setSubmitting(false);
-                }, 500);
-            }
-        }
-        validationSchema={Yup.object().shape({
-            email: Yup.string()
-                .email("Invalid email")
-                .required("Email is required")
-                .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Invalid email"),
-            password: Yup.string()
-                .required("Password is required")
-                .min(6, "Password must be between 6-12 characters")
-                .max(12, "Password must be between 6-12 characters"),
-            password2: Yup.string()
-                .required("Confirm password is required")
-                .oneOf([Yup.ref("password"), null], "Passwords must match")
-        })}
-    >
-        {/* Render props from Formik destructured (ES6) */}
+                })}
+                render={({
+                    values,
+                    errors,
+                    touched,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit
+                }) => (
+                    <ThemeProvider theme={theme}>
+                        <form onSubmit={handleSubmit} className="register-form">
+                            <TextField
+                                name="email"
+                                type="text"
+                                helperText={
+                                    errors.email && touched.email
+                                        ? errors.email
+                                        : "Email"
+                                }
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={errors.email && touched.email}
+                            />
 
-        {props => {
-            const {
-                values,
-                touched,
-                errors,
-                isSubmitting,
-                handleChange,
-                handleBlur,
-                handleSubmit
-            } = props;
-            return (
-                <ThemeProvider theme={theme}>
-                    <form onSubmit={handleSubmit} className="register-form">
-                        <TextField
-                            name="email"
-                            type="text"
-                            helperText={
-                                errors.email && touched.email
-                                    ? errors.email
-                                    : "Email"
-                            }
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            error={errors.email && touched.email}
-                        />
+                            <TextField
+                                name="password"
+                                type="password"
+                                helperText={
+                                    errors.password && touched.password
+                                        ? errors.password
+                                        : "Password"
+                                }
+                                value={values.password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={errors.password && touched.password}
+                            />
 
-                        <TextField
-                            name="password"
-                            type="password"
-                            helperText={
-                                errors.password && touched.password
-                                    ? errors.password
-                                    : "Password"
-                            }
-                            value={values.password}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            error={errors.password && touched.password}
-                        />
+                            <TextField
+                                name="password2"
+                                type="password"
+                                helperText={
+                                    errors.password2 && touched.password2
+                                        ? errors.password2
+                                        : "Confirm password"
+                                }
+                                value={values.password2}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={errors.password2 && touched.password2}
+                            />
 
-                        <TextField
-                            name="password2"
-                            type="password"
-                            helperText={
-                                errors.password2 && touched.password2
-                                    ? errors.password2
-                                    : "Confirm password"
-                            }
-                            value={values.password2}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            error={errors.password2 && touched.password2}
-                        />
-
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            className="button"
-                            type="submit"
-                            disabled={isSubmitting}
-                        >
-                            REGISTER
-                        </Button>
-                    </form>
-                </ThemeProvider>
-            );
-        }}
-    </Formik>
-);
-
-export default RegisterForm;
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className="button"
+                                type="submit"
+                                disabled={isSubmitting}
+                            >
+                                REGISTER
+                            </Button>
+                        </form>
+                    </ThemeProvider>
+                )}
+            />
+        );
+    }
+}
