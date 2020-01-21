2ed9d417153da4ebef6d86653784e8d50716587b
GreenColoredLife <greencoloredlife@gmail.com>
2020-01-22 01:14:30 +0700

    Made Q2 layout

--- react_frontend/src/pages/questions/Q2.js
+++ react_frontend/src/pages/questions/Q2.js
@@ -2,6 +2,9 @@ import React, { useContext, useState } from "react";
 import "../../styles/Q2.css";
 import { QuestionContext } from "../../QuestionContext";
 import BackTab from "../../components/BackTab.js";
+import Button from "@material-ui/core/Button";
+import { ThemeProvider } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export default function Q2() {
     const context = useContext(QuestionContext);
@@ -10,6 +13,14 @@ export default function Q2() {
         <div className="q2">
             <BackTab/>
             <div className="title">Select dish type</div>
+                <Button
+                className="soup"/>
+                <Button
+                className="main"/>
+                <Button
+                className="salad"/>
+                <Button
+                className="dessert"/>
         </div>
     );
 }
